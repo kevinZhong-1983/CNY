62a08d3082f7381cdead0623970edc7beac745a3
kevinZhong-1983 <42631749+kevinZhong-1983@users.noreply.github.com>
2019-01-17 17:46:58 +0800

    Add files via upload

---
+++ src/src/index/gold_MC.js
@@ -0,0 +1,8 @@
+window.index=window.index||{};
+index.gold_MC=function(){
+	var s = this;
+	annie.MovieClip.call(s);
+	/*_a2x_need_start*//*_a2x_need_end*/
+	annie.initRes(s,"index","gold_MC");
+};
+A2xExtend(index.gold_MC,annie.MovieClip);
